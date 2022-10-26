2817d5b37c24ae661b51f6fa92ebadd00228ae7e
Kieran Volz <kieran.volz@gmail.com>
2022-10-26 07:34:23 -0400

    everything works locally
    give it a break

--- mytodolist/src/App.js
+++ mytodolist/src/App.js
@@ -1,25 +1,195 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from "react";
+import { GoFlame } from "https://cdn.skypack.dev/react-icons@4.4.0/go";
+import { FaFeatherAlt } from "https://cdn.skypack.dev/react-icons@4.4.0/fa";
+import { ImMagicWand } from "https://cdn.skypack.dev/react-icons@4.4.0/im";
 import './App.css';
 
+
+                       
+                       
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What Is Thy Bidding?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+
+
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder= "Solemnly Swear I'm Up to No Good"            
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Mischief Managed
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Cast a spell'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Magic <ImMagicWand />
+
+          </button>
+          
+        </>
+      )}
+      
+                
+    </form>
+  );
+}
+
+
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        
+        <div class="tooltip">
+
+           
+        <FaFeatherAlt
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />            
+          
+            <span class="tooltiptext">Edit with quill</span>
+</div>
+
+
+        
+        <div class="tooltip">
+
+        
+        <GoFlame
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        /> 
+        
+  <span class="tooltiptext">Delete with fire</span>
+</div>
+     
+      </div>
+    </div>
+  ));
+};
+
 
 export default App;
